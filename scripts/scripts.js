6b30668dec8e9d292b6ddb3fff5f230e7befed36
LortkipaHub <56543368+LortkipaHub@users.noreply.github.com>
2020-12-11 00:24:21 +0400

    responsive done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,14 @@ handleSearchButtonClick = function(){
     let logo = document.querySelector("#logo");
     let searchFormHiddenButton = document.querySelector("#searchFormHiddenButton");
     let searchFormHidden =  document.querySelector("#searchFormHidden");
+    let menuHeader = document.querySelector('#hiddenMenuHeader');
+    let menu = document.querySelector('#hiddenMenu');
 
     logo.style.display = 'none'
     searchFormHiddenButton.style.display = 'none'
     searchFormHidden.style.display = 'block'
+    menuHeader.style.display = 'none';
+    menu.style.display = 'none';
 
 }
 
@@ -13,10 +17,15 @@ handelCloseButtonClick = function(){
     let logo = document.querySelector("#logo");
     let searchFormHiddenButton = document.querySelector("#searchFormHiddenButton");
     let searchFormHidden =  document.querySelector("#searchFormHidden");
+    let menuHeader = document.querySelector('#hiddenMenuHeader');
+    let menu = document.querySelector('#hiddenMenu');
 
-    logo.style.display = 'block'
-    searchFormHiddenButton.style.display = 'block'
-    searchFormHidden.style.display = 'none'
+    logo.style.display = 'block';
+    searchFormHiddenButton.style.display = 'block';
+    searchFormHidden.style.display = 'none';
+    menuHeader.style.display = 'flex';
+    menu.style.display = 'none';
+    console.log(menuHeader.style.display)
 
 
 
@@ -34,7 +43,26 @@ handleResponsiveSearch = function () {
     
 }
 
+handleResponsiveMenu = function (){
+    let menuHeader = document.querySelector('#hiddenMenuHeader');
+    let menu = document.querySelector('#hiddenMenu');
+    menuHeader.addEventListener('click',function (){
+        if(menu.style.display === "block"){
+            menu.style.display = 'none';
+        }else{
+            menu.style.display = "block";
+        }
+        
+    } )
+
+}
+
+handleResponsive=function(){
+    handleResponsiveSearch();
+    handleResponsiveMenu();
+}
 
 
 
-window.onload=handleResponsiveSearch;
+
+window.onload=handleResponsive;
